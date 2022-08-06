afcf28f46fef944cafc771d3647dc435ba3ef9d1
Saša Stevanović <sstevano@amazon.com>
2022-08-05 18:16:11 -0700

    Dodao praktično ručni keš, to jest slike sačuvam u fajlu, a pre nego što učitavam sa interneta probam lokalni fajl. Ovo je slično kao što radi na Androidu. Dodao spinner dok se učitava, testirao sve i izgleda baš prijatno radi.

--- MasterDetailTest/OnePageController.swift
+++ MasterDetailTest/OnePageController.swift
@@ -11,6 +11,7 @@ import UIKit
 class OnePageController : UIViewController {
     var page: (Int, String) = (-1, "") {
         didSet {
+            fileNameSuffix = OnePageController.lastChunk(from: page.1, startingWith: "/")
             preload()
         }
     }
@@ -18,26 +19,36 @@ class OnePageController : UIViewController {
     static var lastLoadedIndex: Int = -1
     var task: URLSessionDataTask?
     var image: UIImage?
+    var fileNameSuffix: String = ""
     var didLoad: Bool = false // možda je ovo isto kao isViewLoaded a možda i nije, pa za svaki slučaj
-
-    static let urlSession: URLSession = {
-        // let fileManagerUrl = try FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: .none, create: true)
-        let config = URLSessionConfiguration.default
-        config.urlCache = URLCache(memoryCapacity: 6*1024*1024, diskCapacity: 100*1024*1024, diskPath: "pages")
-        config.requestCachePolicy = .returnCacheDataElseLoad
-        return URLSession(configuration: config)
+    static let cacheDir: URL? = {
+        let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        if dir != nil && !FileManager.default.fileExists(atPath: dir!.path) {
+            try? FileManager.default.createDirectory(at: dir!, withIntermediateDirectories: true)
+        }
+        return dir
     }()
 
+    static func lastChunk(from s: String, startingWith c: Character) -> String {
+        guard let pos = s.lastIndex(of: c) else {
+            return ""
+        }
+        return String(s[pos...])
+    }
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if page.0 == -1 {
+            self.activityIndicator.stopAnimating()
             return
         }
         didLoad = true
         if let image = image {
             self.imageView.image = image
+            self.activityIndicator.stopAnimating()
         }
     }
     
@@ -45,34 +56,74 @@ class OnePageController : UIViewController {
         if page.0 == -1 {
             return
         }
-        let urlRequest = URLRequest(url: URL(string: page.1)!, cachePolicy: .returnCacheDataElseLoad)
-        task = OnePageController.urlSession.dataTask(with: urlRequest) { data, response, error in
+        guard !fileNameSuffix.isEmpty else {
+            return
+        }
+        guard let cacheDir = OnePageController.cacheDir else {
+            startDownloading()
+            return
+        }
+
+        let file = cacheDir.path + fileNameSuffix
+        if !FileManager.default.fileExists(atPath: file) {
+            startDownloading()
+            return
+        }
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let image = UIImage(contentsOfFile: file) {
+                self.updateView(with: image)
+            } else {
+                // Should never happen
+                self.startDownloading()
+                try? FileManager.default.removeItem(atPath: file)
+            }
+        }
+    }
+    
+    func startDownloading() {
+        let urlRequest = URLRequest(url: URL(string: page.1)!)
+        task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             if let error = error {
                 // self.handleClientError(error)
+                if self.didLoad {
+                    self.activityIndicator.stopAnimating()
+                }
                 return
             }
             guard let httpResponse = response as? HTTPURLResponse,
                 (200...299).contains(httpResponse.statusCode) else {
                 // self.handleServerError(response)
+                if self.didLoad {
+                    self.activityIndicator.stopAnimating()
+                }
                 return
             }
-            if let response = response,
-               let data = data,
+            if let data = data,
                let image = UIImage(data: data) {
-                if let urlCache = OnePageController.urlSession.configuration.urlCache,
-                   urlCache.cachedResponse(for: urlRequest) == nil {
-                    urlCache.storeCachedResponse(CachedURLResponse(response: response, data: data, storagePolicy: .allowed), for: urlRequest)
-                }
-                DispatchQueue.main.async {
-                    if self.didLoad {
-                        self.imageView.image = image
-                    } else {
-                        self.image = image
+                self.updateView(with: image)
+                if let cacheDir = OnePageController.cacheDir {
+                    let file = cacheDir.path + self.fileNameSuffix
+                    let tmpFile = file + ".tmp"
+                    DispatchQueue.global(qos: .utility).async {
+                        if FileManager.default.createFile(atPath: tmpFile, contents: data) {
+                            try? FileManager.default.moveItem(atPath: tmpFile, toPath: file)
+                        }
                     }
                 }
             }
         }
         task!.resume()
+    }
+
+    func updateView(with image: UIImage) {
+        DispatchQueue.main.async {
+            if self.didLoad {
+                self.imageView.image = image
+                self.activityIndicator.stopAnimating()
+            } else {
+                self.image = image
+            }
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
